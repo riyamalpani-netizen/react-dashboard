f596b5ecc9f34de31090069ac89be1026d335dad
Riya malpani <riya.malpani@xtenav.com>
2026-08-03 17:41:43 +0530

    Add state management for Signup form inputs

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,13 @@
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";  //it is used to store and update the email and the password.
 
 function Signup() {
   const navigate = useNavigate(); //used to navigate to the login page after signup
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   const handleSignup = () => {  //runs when the signup button is clicked
     navigate("/login"); //navigates to the login page after signup
   };
@@ -15,24 +21,32 @@ function Signup() {
         <input
           type="text"
           placeholder="Full Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
           className="w-full border border-gray-300 rounded-lg p-3 mb-4"
         />
 
         <input
           type="email"
           placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           className="w-full border border-gray-300 rounded-lg p-3 mb-4"
         />
 
         <input
           type="password"
           placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           className="w-full border border-gray-300 rounded-lg p-3 mb-4"
         />
 
         <input
           type="password"
           placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
           className="w-full border border-gray-300 rounded-lg p-3 mb-6"
         />
         <button
