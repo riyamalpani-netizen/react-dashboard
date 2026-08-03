9d5333959953b6bb17a4a73a6c8143b24fe07241
Riya malpani <riya.malpani@xtenav.com>
2026-08-03 17:09:37 +0530

    Refactor DashboardCards to receive cards as props and update Dashboard to manage state

--- src/components/DashboardCards.jsx
+++ src/components/DashboardCards.jsx
@@ -1,17 +1,10 @@
-function DashboardCards() {
-  const cards = [
-    { title: "Total Users", value: 150 },  //static data for the dashboard cards
-    { title: "Orders", value: 82 },
-    { title: "Revenue", value: "₹1,25,000" },
-    { title: "Pending", value: 15 },
-  ];
-
+function DashboardCards({ cards }) { //here it is recieving the cards through props from the parent component Dashboard.jsx
   return (
     <div className="grid grid-cols-4 gap-4 mb-8">
       {cards.map((card, index) => (
-        <div key={index} className="bg-white rounded-lg shadow p-5">
+        <div key={index} className="bg-white rounded-lg shadow p-5 hover:shadow-xl transition duration-300">
           <h3 className="text-gray-600">{card.title}</h3>
-          <p className="text-3xl font-bold mt-2">{card.value}</p>
+          <p className="text-3xl font-bold mt-2 text-blue-600">{card.value}</p>
         </div>
       ))}
     </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,25 @@ import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import DashboardCards from "../components/DashboardCards";
 import UserTable from "../components/UserTable";
+import { useState, useEffect } from "react";
 function Dashboard() {
+  const [cards, setCards] = useState([
+    { title: "Total Users", value: 150 },
+    { title: "Orders", value: 82 },
+    { title: "Revenue", value: "₹1,25,000" },
+    { title: "Pending", value: 15 },
+  ]); //used react hooks below 
+
+  const [users, setUsers] = useState([
+    { id: 1, name: "Riya", email: "riya@gmail.com" },
+    { id: 2, name: "Shruti", email: "shruti@gmail.com" },
+    { id: 3, name: "Aditya", email: "aditya@gmail.com" },
+  ]);
+
+ 
+  useEffect(() => {
+    console.log("Dashboard Loaded Successfully");
+  }, []);
   return (
     <>
       <Navbar />
@@ -11,8 +29,8 @@ function Dashboard() {
         <Sidebar />
 
         <div className="flex-1 p-6 bg-gray-100 min-h-screen">
-          <DashboardCards />
-          <UserTable />
+          <DashboardCards cards={cards} />
+          <UserTable users={users} />
         </div>
       </div>
     </>
